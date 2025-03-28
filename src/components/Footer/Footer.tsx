e22306563f367e24a8e17a3129b7e4761915e804
MahmoudNasser1242000 <mahmoudahmed102030405060@gmail.com>
2025-03-28 07:59:27 +0200

    final footer updates

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -19,7 +19,7 @@ const Footer = () => {
     const { resolvedTheme } = useTheme();
     const [themes, setTheme] = useState<string>("");
     const [pathValid, setPathValid] = useState<boolean>();
-    const {user} = useUser();
+    const { user } = useUser();
     const path = usePathname();
 
     useEffect(() => {
@@ -39,31 +39,29 @@ const Footer = () => {
     }, [path]);
 
     return <>
-        <footer className={`bg-white dark:bg-black border-t border-gray-200 dark:border-gray-800 mt-28 lg:mt-28 ${user ? "block" : (!user && pathValid)? "block" : "hidden"}`}>
+        <footer className={`bg-white dark:bg-black border-t border-gray-200 dark:border-gray-800 mt-28 lg:mt-28 ${user ? "block" : (!user && pathValid) ? "block" : "hidden"}`}>
             <div className="mx-auto max-w-5xl px-4 py-8 sm:px-6 lg:px-8">
                 <div className="flex justify-center text-teal-600">
-                    <h1 className="flex items-center gap-4 text-3xl">
-                        <Link href={"/"}>
-                            <Image
-                                src={`/images/${themes === "dark" ? "logo-dark.svg" : "logo.svg"}`}
-                                alt="Logo"
-                                width={80}
-                                height={80}
-                                className="object-contain"
-                            />
-                            <span className="font-bold text-gray-800 dark:text-gray-100 font-mono">
-                                My Store
-                            </span>
-                        </Link>
-                    </h1>
+                    <Link href={"/"} className="flex items-center gap-4 text-3xl">
+                        <Image
+                            src={`/images/${themes === "dark" ? "logo-dark.svg" : "logo.svg"}`}
+                            alt="Logo"
+                            width={80}
+                            height={80}
+                            className="object-contain"
+                        />
+                        <span className="font-bold text-gray-800 dark:text-gray-100 font-mono">
+                            My Store
+                        </span>
+                    </Link>
                 </div>
 
                 <p className="mx-auto mt-6 max-w-lg text-center leading-relaxed text-gray-500">
                     Shop with confidence at [My Store], where quality meets convenience. Enjoy secure shopping, fast shipping, and top-notch customer service. Thank you for choosing us!
                 </p>
 
-                <ul className={`mt-6 flex flex-wrap justify-center ${!user? "gap-5" : "gap-6 md:gap-8 lg:gap-12"}`}>
-                    {navLinks.filter(({ label }: { label: string }) => user? true : (label !== "Cart")).map(({ href, label }: INavLinks) => (
+                <ul className={`mt-6 flex flex-wrap justify-center ${!user ? "gap-5" : "gap-6 md:gap-8 lg:gap-12"}`}>
+                    {navLinks.filter(({ label }: { label: string }) => user ? true : (label !== "Cart")).map(({ href, label }: INavLinks) => (
                         <li key={href}>
                             <Link
                                 href={href}
